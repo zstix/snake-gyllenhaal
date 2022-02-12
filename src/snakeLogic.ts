de9d064533ec9286b91a6333532b6da5c1549590
Zack Stickles <zstickles@newrelic.com>
2022-02-11 16:52:39 -0800

    chore: finding the closest food

--- src/snakeLogic.ts
+++ src/snakeLogic.ts
@@ -1,53 +1,65 @@
-import { Board, Direction, Position, GameState, Battlesnake } from './types';
-import { randomItem } from './utils';
+import { Board, Direction, Position, GameState, Battlesnake } from "./types";
+import { randomItem, getDistance } from "./utils";
 
 const MOVES: Direction[] = ["left", "right", "up", "down"];
 
-const getNextMove = ({ x, y}: Position, dir: Direction): Position => {
+const getNextMove = ({ x, y }: Position, dir: Direction): Position => {
   const moves: Record<Direction, Position> = {
-    "left":  { x: x - 1, y },
-    "right": { x: x + 1, y },
-    "down":  { x, y: y - 1 },
-    "up":    { x, y: y + 1 },
+    left: { x: x - 1, y },
+    right: { x: x + 1, y },
+    down: { x, y: y - 1 },
+    up: { x, y: y + 1 },
   };
 
   return moves[dir];
-}
+};
 
 const bodyContainsPos = (body: Position[], pos: Position): boolean =>
-  body.some((segment) => (segment.x == pos.x && segment.y == pos.y))
+  body.some((segment) => segment.x == pos.x && segment.y == pos.y);
 
-const notWalls = (board: Board, pos: Position) => (dir: Direction): boolean => {
-  const { width, height } = board;
-  const { x, y } = pos;
+const notWalls =
+  (board: Board, pos: Position) =>
+  (dir: Direction): boolean => {
+    const { width, height } = board;
+    const { x, y } = pos;
 
-  const moves: Record<Direction, boolean> = {
-    "left": x > 0,
-    "right": x < width - 1,
-    "down": y > 0,
-    "up": y < height - 1
+    const moves: Record<Direction, boolean> = {
+      left: x > 0,
+      right: x < width - 1,
+      down: y > 0,
+      up: y < height - 1,
+    };
+
+    return moves[dir];
   };
 
-  return moves[dir];
-}
+const notSnakeBodies =
+  (snakes: Battlesnake[], pos: Position) =>
+  (dir: Direction): boolean =>
+    snakes.reduce(
+      (okay, { body }) => okay && !bodyContainsPos(body, getNextMove(pos, dir)),
+      true
+    );
 
-const notSnakeBodies = (snakes: Battlesnake[], pos: Position) => (dir: Direction): boolean =>
-  snakes.reduce((okay, { body }) => okay && !bodyContainsPos(body, getNextMove(pos, dir)), true);
+const getClosestFood = (food: Position[], head: Position): Position =>
+  food.sort((a, b) => getDistance(a, head) - getDistance(b, head))[0];
 
 export const chooseMove = (state: GameState, debug = false): Direction => {
   const { board, you } = state;
   const { head } = you;
   const { snakes } = board;
 
-  const possibleMoves = MOVES
-    .filter(notWalls(board, head))
-    .filter(notSnakeBodies(snakes, head));
+  const possibleMoves = MOVES.filter(notWalls(board, head)).filter(
+    notSnakeBodies(snakes, head)
+  );
 
   const chosenMove = randomItem(possibleMoves);
 
   if (debug) {
-    console.log(`${state.game.id} MOVE ${state.turn}: ${chosenMove} (out of ${possibleMoves})`);
+    console.log(
+      `${state.game.id} MOVE ${state.turn}: ${chosenMove} (out of ${possibleMoves})`
+    );
   }
-  
+
   return chosenMove;
-}
+};
--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,5 @@
+import { Position } from "./types";
+
 export const unique = <T>(arr: T[]): T[] => [...new Set(arr)];
 
 export const range = (start: number, end?: number): number[] => {
@@ -6,8 +8,11 @@ export const range = (start: number, end?: number): number[] => {
     start = 0;
   }
 
-  return Array.from({ length: end - start + 1}, (_, i) => i);
-}
+  return Array.from({ length: end - start + 1 }, (_, i) => i);
+};
 
 export const randomItem = <T>(arr: T[]): T =>
   arr[Math.floor(Math.random() * arr.length)];
+
+export const getDistance = (a: Position, b: Position): number =>
+  Math.sqrt(Math.pow(b.x - a.x, 2) + Math.pow(b.y - a.y, 2));
