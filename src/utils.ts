9739de8f526809f1957f4fc3d913e019d364ff2d
Zack Stickles <zstickles@newrelic.com>
2022-02-14 09:15:32 -0800

    chore: added a pipe function

--- src/utils.ts
+++ src/utils.ts
@@ -1,5 +1,3 @@
-import { Position } from "./types";
-
 export const unique = <T>(arr: T[]): T[] => [...new Set(arr)];
 
 export const range = (start: number, end?: number): number[] => {
@@ -14,13 +12,22 @@ export const range = (start: number, end?: number): number[] => {
 export const randomItem = <T>(arr: T[]): T =>
   arr[Math.floor(Math.random() * arr.length)];
 
-export const getDistance = (a: Position, b: Position): number =>
-  Math.sqrt(Math.pow(b.x - a.x, 2) + Math.pow(b.y - a.y, 2));
-
 export const shuffle = <T>(arr: T[]): T[] => {
   for (let i = arr.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
     [arr[i], arr[j]] = [arr[j], arr[i]];
   }
   return arr;
+};
+
+export const pipe = <T extends any[], R>(
+  fn1: (...args: T) => R,
+  ...fns: Array<(a: R) => R>
+) => {
+  const piped = fns.reduce(
+    (prevFn, nextFn) => (value: R) => nextFn(prevFn(value)),
+    (value) => value
+  );
+
+  return (...args: T) => piped(fn1(...args));
 };
